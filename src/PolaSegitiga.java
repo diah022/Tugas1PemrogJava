0f5902540ac8e4bc164ad07383cdbc0d79e47c26
diah022 <lee.diah05@gmail.com>
2016-11-04 00:50:06 +0700

    12141363 perulangan yang menunjuk batasan

--- src/PolaSegitiga.java
+++ src/PolaSegitiga.java
@@ -46,7 +46,15 @@ public class PolaSegitiga {
     private void SegitgaKanan() {
         inputBatasan(batasan);
         
-        
+        for(int i = 0; i <= 6; i++)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+            i++;
+        }
     }
 
     private void SegitigaTerbalikKanan() {
